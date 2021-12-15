2816c8bc4f66aa59fec0639b2d6b209b1a71a463
Stuyk <Stuyk@users.noreply.github.com>
2021-12-15 21:53:22 +0000

    Deploying to dist from @ Stuyk/altv-athena-plugins@a6aaa49cfcfe345956bb2c91f891c45b64329192 🚀

---
+++ assets/plantsystem.55c281c9.js
@@ -0,0 +1,37 @@
+import{_ as t}from"./preload-helper.8b70a8dd.js";import{k as l}from"./vendor.d14f5e04.js";l("2mxhQ",{value:!0},async()=>{const[{default:e},{default:n}]=await Promise.all([t(()=>import("./MainDisplay.9c2c0444.js"),["assets/MainDisplay.9c2c0444.js","assets/Header.cb8181cd.js","assets/vendor.d14f5e04.js"]),t(()=>import("./client.1c2c108f.js"),["assets/client.1c2c108f.js","assets/vendor.d14f5e04.js"])]);return(o,a)=>n(o)(e,{content:{title:"Athena Plantsystem",description:"Plant system which lets players place weed plants and also harvest them based on Athena's Interaction Controllers.",author:"Der Lord!",version:"3.0.0+",images:["https://user-images.githubusercontent.com/82890183/131876685-13775cce-d8ee-4eb5-b95e-b3ad8520a3cf.png"],keywords:["free","player","system"],url:"https://github.com/Booster1212/AthenaPlantsystem",astro:{headers:[{depth:1,slug:"example-free-plugin",text:"Example Free Plugin"},{depth:1,slug:"features",text:"Features:"},{depth:1,slug:"installation-instructions",text:"Installation Instructions"}],source:`
+# Example Free Plugin
+
+# Features:
+
+- Now fully translateable into your native language
+- Now based on Athena's InteractionController
+- Now you can still plant weed pots by Toolbar but now for fertilizing, watering and so on you'll only need to have stuff in inventory.
+- Now with maximum Plant support for each player individually
+- Customize scenarios & the time for each.
+- Customize the updateInterval & also the states of the plants.
+- Customize the range to a spot players can have
+- Customizie the outcome of the plant harvesting (only tried randomized outcome there so far)
+- Customize the water loss per minute .. and a lot more.
+
+# Installation Instructions
+
+1. Grab from github (https://github.com/Booster1212/AthenaPlantsystem)
+2. Put folder into /src/core/plugins
+`,html:`<h1 id="example-free-plugin">Example Free Plugin</h1>
+<h1 id="features">Features:</h1>
+<ul>
+<li>Now fully translateable into your native language</li>
+<li>Now based on Athena\u2019s InteractionController</li>
+<li>Now you can still plant weed pots by Toolbar but now for fertilizing, watering and so on you\u2019ll only need to have stuff in inventory.</li>
+<li>Now with maximum Plant support for each player individually</li>
+<li>Customize scenarios &#x26; the time for each.</li>
+<li>Customize the updateInterval &#x26; also the states of the plants.</li>
+<li>Customize the range to a spot players can have</li>
+<li>Customizie the outcome of the plant harvesting (only tried randomized outcome there so far)</li>
+<li>Customize the water loss per minute .. and a lot more.</li>
+</ul>
+<h1 id="installation-instructions">Installation Instructions</h1>
+<ol>
+<li>Grab from github (<a href="https://github.com/Booster1212/AthenaPlantsystem">https://github.com/Booster1212/AthenaPlantsystem</a>)</li>
+<li>Put folder into /src/core/plugins</li>
+</ol>`}}},a)});
