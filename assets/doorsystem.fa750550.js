42fc1d5be001820f81057b29326d43da82c17062
Stuyk <Stuyk@users.noreply.github.com>
2021-12-17 17:43:43 +0000

    Deploying to dist from @ Stuyk/altv-athena-plugins@eefce459217386cf54602875968f17bef0a4d82d 🚀

--- assets/doorsystem.fa750550.js
+++ assets/doorsystem.fa750550.js
@@ -1,4 +1,4 @@
-import{_ as e}from"./preload-helper.8b70a8dd.js";import{k as s}from"./vendor.3220ec20.js";s("ZCggsI",{value:!0},async()=>{const[{default:n},{default:t}]=await Promise.all([e(()=>import("./MainDisplay.cbca4de0.js"),["assets/MainDisplay.cbca4de0.js","assets/Header.f251eaf5.js","assets/vendor.3220ec20.js"]),e(()=>import("./client.1eab480f.js"),["assets/client.1eab480f.js","assets/vendor.3220ec20.js"])]);return(o,a)=>t(o)(n,{content:{title:"Athena Doorlock System",description:"Doorsystem which enables you to lock/unlock door based on a player or faction.",author:"Der Lord!",version:"3.0.0+",images:["https://user-images.githubusercontent.com/82890183/136729801-b86dc411-56e7-4d90-b8fb-a308143823b4.png"],keywords:["free","player","system","streamer","client"],url:"https://github.com/Booster1212/AthenaDoorlock",astro:{headers:[{depth:3,slug:"features",text:"Features"},{depth:3,slug:"how-to-use",text:"HOW TO USE"},{depth:3,slug:"setup",text:"SETUP"}],source:`
+import{_ as e}from"./preload-helper.8b70a8dd.js";import{k as s}from"./vendor.d14f5e04.js";s("Z1hyBLC",{value:!0},async()=>{const[{default:n},{default:t}]=await Promise.all([e(()=>import("./MainDisplay.33a33a20.js"),["assets/MainDisplay.33a33a20.js","assets/Header.3318a738.js","assets/vendor.d14f5e04.js"]),e(()=>import("./client.1c2c108f.js"),["assets/client.1c2c108f.js","assets/vendor.d14f5e04.js"])]);return(o,a)=>t(o)(n,{content:{title:"Door Lock System",description:"Door system which enables you to lock/unlock door based on a player or faction.",author:"Der Lord!",version:"3.0.0+",images:["https://user-images.githubusercontent.com/82890183/136729801-b86dc411-56e7-4d90-b8fb-a308143823b4.png"],keywords:["free","player","system","streamer","client"],url:"https://github.com/Booster1212/AthenaDoorlock",discord:"Der Lord!#6021",astro:{headers:[{depth:3,slug:"features",text:"Features"},{depth:3,slug:"how-to-use",text:"HOW TO USE"},{depth:3,slug:"setup",text:"SETUP"}],source:`
 ### Features
 * Add Doors Ingame
 * Remove Doors Ingame
@@ -18,12 +18,14 @@ import{_ as e}from"./preload-helper.8b70a8dd.js";import{k as s}from"./vendor.322
 - Add \u201Cdoors\u201D to your MONGO_COLLECTIONS= in the .env file.
 
 imports.ts (Clientside)
-\`\`\`typescript 
+
+\`\`\`ts 
 import './AthenaDoorlock/main';
 import './AthenaDoorlock/wheelmenu';
 \`\`\`
+
 imports.ts (Serverside)
-\`\`\`typescript
+\`\`\`ts
 './AthenaDoorlock/main',
 './AthenaDoorlock/streamer',
 './AthenaDoorlock/interface',
@@ -50,9 +52,9 @@ imports.ts (Serverside)
 <li>Add \u201Cdoors\u201D to your MONGO_COLLECTIONS= in the .env file.</li>
 </ul>
 <p>imports.ts (Clientside)</p>
-<pre class="language-typescript"><code data-astro-raw class="language-typescript"><span class="token keyword">import</span> <span class="token string">'./AthenaDoorlock/main'</span><span class="token punctuation">;</span>
+<pre class="language-ts"><code data-astro-raw class="language-ts"><span class="token keyword">import</span> <span class="token string">'./AthenaDoorlock/main'</span><span class="token punctuation">;</span>
 <span class="token keyword">import</span> <span class="token string">'./AthenaDoorlock/wheelmenu'</span><span class="token punctuation">;</span></code></pre>
 <p>imports.ts (Serverside)</p>
-<pre class="language-typescript"><code data-astro-raw class="language-typescript"><span class="token string">'./AthenaDoorlock/main'</span><span class="token punctuation">,</span>
+<pre class="language-ts"><code data-astro-raw class="language-ts"><span class="token string">'./AthenaDoorlock/main'</span><span class="token punctuation">,</span>
 <span class="token string">'./AthenaDoorlock/streamer'</span><span class="token punctuation">,</span>
 <span class="token string">'./AthenaDoorlock/interface'</span><span class="token punctuation">,</span></code></pre>`}}},a)});
---
+++ assets/interior-plugin.305ebf5f.js
@@ -0,0 +1,77 @@
+import{_ as n}from"./preload-helper.8b70a8dd.js";import{k as r}from"./vendor.d14f5e04.js";r("en0R5",{value:!0},async()=>{const[{default:s},{default:t}]=await Promise.all([n(()=>import("./MainDisplay.33a33a20.js"),["assets/MainDisplay.33a33a20.js","assets/Header.3318a738.js","assets/vendor.d14f5e04.js"]),n(()=>import("./client.1c2c108f.js"),["assets/client.1c2c108f.js","assets/vendor.d14f5e04.js"])]);return(a,o)=>t(a)(s,{content:{title:"Interior Plugin",description:"This Plugin helps you to find an interior including position and ipl informations",author:"j0n4s",version:"3.0.0+",images:["https://i.imgur.com/K4QjdzE.png"],keywords:["free","client","server","data"],url:"https://github.com/jonasesser/altv-athena-plugin-interior",discord:"j0n4s#8301",astro:{headers:[{depth:1,slug:"interior-plugin",text:"Interior Plugin"},{depth:1,slug:"installation-instructions",text:"Installation Instructions"},{depth:1,slug:"usage",text:"Usage"}],source:`\r
+# Interior Plugin\r
+\r
+This Plugin helps you to find an interior including position and ipl informations.\r
+You can use it ingame as admin or for development as interior database.\r
+\r
+# Installation Instructions\r
+\r
+1. Grab from github\r
+2. Copy Folder plugins/gpInteriors/ to your athena project under src/core/plugins/\r
+3. Import server plugin in src/core/plugins/imports.ts:\r
+\r
+    \`import './gpInteriors/index';\`\r
+\r
+# Usage\r
+\r
+1. In-Game as administrator\r
+\r
+    \`/gotointerior [name] - Goto Interior (try /gotointerior Movie Theatre)\`\r
+\r
+2. In-Code as developer\r
+\r
+\`\`\`ts\r
+import { GpInteriors, GpInteriors_Category } from '../gpInteriors/gpInteriors';\r
+\r
+// Get Interiors    \r
+let interior = GpInteriors.getInterior("Movie Theatre");\r
+let allInteriors = GpInteriors.getInteriors();\r
+let interiorByCat = GpInteriors.getInteriorsByCategory(GpInteriors_Category.Office);\r
+\r
+// Get Interior names only\r
+let allInteriorNames = GpInteriors.getInteriorNames();\r
+let interiorNamesByCat = GpInteriors.getInteriorNamesByCategory(GpInteriors_Category.Office);\r
+\r
+// Get Interior position by name\r
+let position = GpInteriors.getInteriorPosition(interior.Name);  //returns alt.Vector3\r
+let ipl = GpInteriors.getInteriorIPL(interior.Name); //returns IPL name to be load or null\r
+\`\`\``,html:`<h1 id="interior-plugin">Interior Plugin</h1>
+<p>This Plugin helps you to find an interior including position and ipl informations.\r
+You can use it ingame as admin or for development as interior database.</p>
+<h1 id="installation-instructions">Installation Instructions</h1>
+<ol>
+<li>
+<p>Grab from github</p>
+</li>
+<li>
+<p>Copy Folder plugins/gpInteriors/ to your athena project under src/core/plugins/</p>
+</li>
+<li>
+<p>Import server plugin in src/core/plugins/imports.ts:</p>
+<p><code data-astro-raw>import './gpInteriors/index';</code></p>
+</li>
+</ol>
+<h1 id="usage">Usage</h1>
+<ol>
+<li>
+<p>In-Game as administrator</p>
+<p><code data-astro-raw>/gotointerior [name] - Goto Interior (try /gotointerior Movie Theatre)</code></p>
+</li>
+<li>
+<p>In-Code as developer</p>
+</li>
+</ol>
+<pre class="language-ts"><code data-astro-raw class="language-ts"><span class="token keyword">import</span> <span class="token punctuation">{</span> GpInteriors<span class="token punctuation">,</span> GpInteriors_Category <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">'../gpInteriors/gpInteriors'</span><span class="token punctuation">;</span>\r
+\r
+<span class="token comment">// Get Interiors    </span>\r
+<span class="token keyword">let</span> interior <span class="token operator">=</span> GpInteriors<span class="token punctuation">.</span><span class="token function">getInterior</span><span class="token punctuation">(</span><span class="token string">"Movie Theatre"</span><span class="token punctuation">)</span><span class="token punctuation">;</span>\r
+<span class="token keyword">let</span> allInteriors <span class="token operator">=</span> GpInteriors<span class="token punctuation">.</span><span class="token function">getInteriors</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>\r
+<span class="token keyword">let</span> interiorByCat <span class="token operator">=</span> GpInteriors<span class="token punctuation">.</span><span class="token function">getInteriorsByCategory</span><span class="token punctuation">(</span>GpInteriors_Category<span class="token punctuation">.</span>Office<span class="token punctuation">)</span><span class="token punctuation">;</span>\r
+\r
+<span class="token comment">// Get Interior names only</span>\r
+<span class="token keyword">let</span> allInteriorNames <span class="token operator">=</span> GpInteriors<span class="token punctuation">.</span><span class="token function">getInteriorNames</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>\r
+<span class="token keyword">let</span> interiorNamesByCat <span class="token operator">=</span> GpInteriors<span class="token punctuation">.</span><span class="token function">getInteriorNamesByCategory</span><span class="token punctuation">(</span>GpInteriors_Category<span class="token punctuation">.</span>Office<span class="token punctuation">)</span><span class="token punctuation">;</span>\r
+\r
+<span class="token comment">// Get Interior position by name</span>\r
+<span class="token keyword">let</span> position <span class="token operator">=</span> GpInteriors<span class="token punctuation">.</span><span class="token function">getInteriorPosition</span><span class="token punctuation">(</span>interior<span class="token punctuation">.</span>Name<span class="token punctuation">)</span><span class="token punctuation">;</span>  <span class="token comment">//returns alt.Vector3</span>\r
+<span class="token keyword">let</span> ipl <span class="token operator">=</span> GpInteriors<span class="token punctuation">.</span><span class="token function">getInteriorIPL</span><span class="token punctuation">(</span>interior<span class="token punctuation">.</span>Name<span class="token punctuation">)</span><span class="token punctuation">;</span> <span class="token comment">//returns IPL name to be load or null</span></code></pre>`}}},o)});
