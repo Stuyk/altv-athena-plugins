1e819b4b58391bf0323a1d20a40ae91bf7a3bdb2
Stuyk <Stuyk@users.noreply.github.com>
2021-12-26 01:45:15 +0000

    Deploying to dist from @ Stuyk/altv-athena-plugins@8478f5d3b0cfe38de4f96ce0218e8b59929f83bd 🚀

---
+++ assets/portal-plugin.0c04425b.js
@@ -0,0 +1,73 @@
+import{_ as r}from"./preload-helper.8b70a8dd.js";import{l as o}from"./vendor.7d2999dd.js";o("1AGMGO",{value:!0},async()=>{const[{default:e},{default:t}]=await Promise.all([r(()=>import("./MainDisplay.a53a1701.js"),["assets/MainDisplay.a53a1701.js","assets/Header.b4b763a5.js","assets/vendor.7d2999dd.js"]),r(()=>import("./client.e66485df.js"),["assets/client.e66485df.js","assets/vendor.7d2999dd.js"])]);return(n,a)=>t(n)(e,{content:{title:"Portal Plugin",description:"This Plugin creates portal gates where the player can port or beam from one place to another.",author:"j0n4s",version:"3.0.0+",images:["https://i.imgur.com/ChQzzPI.png"],keywords:["free","client","server","data"],url:"https://github.com/jonasesser/altv-athena-plugin-interior",discord:"j0n4s#8301",astro:{headers:[{depth:1,slug:"portal-plugin",text:"Portal Plugin"},{depth:1,slug:"features",text:"Features"},{depth:1,slug:"features-in-progress",text:"Features in progress"},{depth:1,slug:"installation",text:"Installation"},{depth:1,slug:"usage",text:"Usage"}],source:`\r
+# Portal Plugin\r
+\r
+This Plugin creates portals where the player can port/beam from one place to another.\r
+\r
+# Features\r
+\r
+- Create Portal with two gates\r
+\r
+# Features in progress\r
+\r
+- Create Portals with multiple gates (e.g. for lifts)\r
+- Player menu\r
+- Admin menu\r
+- Dimension and IPL Support\r
+\r
+# Installation\r
+\r
+1. Get plugin here\r
+\r
+https://mygp.gumroad.com/l/portalplugin\r
+    \r
+2. Copy Folder plugins/gpPortal/ to your athena project under src/core/plugins/\r
+3. Import server plugin in src/core/plugins/imports.ts:\r
+\r
+    \`\`\`import './gpPortal/index';\`\`\`\r
+\r
+# Usage\r
+\r
+1. In-Game as administrator\r
+\r
+    \`\`\`/addportal [name] - Adds a portal at current position\`\`\`\r
+\r
+2. See video\r
+\r
+https://youtu.be/7Nygyl0uFA0`,html:`<h1 id="portal-plugin">Portal Plugin</h1>
+<p>This Plugin creates portals where the player can port/beam from one place to another.</p>
+<h1 id="features">Features</h1>
+<ul>
+<li>Create Portal with two gates</li>
+</ul>
+<h1 id="features-in-progress">Features in progress</h1>
+<ul>
+<li>Create Portals with multiple gates (e.g. for lifts)</li>
+<li>Player menu</li>
+<li>Admin menu</li>
+<li>Dimension and IPL Support</li>
+</ul>
+<h1 id="installation">Installation</h1>
+<ol>
+<li>Get plugin here</li>
+</ol>
+<p><a href="https://mygp.gumroad.com/l/portalplugin">https://mygp.gumroad.com/l/portalplugin</a></p>
+<ol start="2">
+<li>
+<p>Copy Folder plugins/gpPortal/ to your athena project under src/core/plugins/</p>
+</li>
+<li>
+<p>Import server plugin in src/core/plugins/imports.ts:</p>
+<p><code data-astro-raw>import './gpPortal/index';</code></p>
+</li>
+</ol>
+<h1 id="usage">Usage</h1>
+<ol>
+<li>
+<p>In-Game as administrator</p>
+<p><code data-astro-raw>/addportal [name] - Adds a portal at current position</code></p>
+</li>
+<li>
+<p>See video</p>
+</li>
+</ol>
+<p><a href="https://youtu.be/7Nygyl0uFA0">https://youtu.be/7Nygyl0uFA0</a></p>`}}},a)});
