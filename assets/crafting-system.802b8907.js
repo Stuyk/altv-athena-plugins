d0ec05ec4bc34d2a10dd2604bbd305f9e8c41410
Stuyk <Stuyk@users.noreply.github.com>
2022-01-03 13:28:37 +0000

    Deploying to dist from @ Stuyk/altv-athena-plugins@4483b54b3af826bda1df51d8a5c06370d9c5c6de 🚀

---
+++ assets/crafting-system.802b8907.js
@@ -0,0 +1,30 @@
+import{_ as t}from"./preload-helper.8b70a8dd.js";import{l as r}from"./vendor.7d2999dd.js";r("oM232",{value:!0},async()=>{const[{default:e},{default:i}]=await Promise.all([t(()=>import("./MainDisplay.12b9d463.js"),["assets/MainDisplay.12b9d463.js","assets/Header.47e3a24d.js","assets/vendor.7d2999dd.js"]),t(()=>import("./client.e66485df.js"),["assets/client.e66485df.js","assets/vendor.7d2999dd.js"])]);return(s,n)=>i(s)(e,{content:{title:"Crafting System Premium Plugin",description:"Crafting Spots and Crafting Items",author:"SeipekM",version:"3.0.0+",images:["https://i.imgur.com/XXdxm6i.png"],keywords:["premium","server","player","system"],url:"https://seipekm.gumroad.com/l/byzby",price:"15.00",discord:"seipekm#0300",astro:{headers:[{depth:1,slug:"crafting-system-premium-plugin",text:"Crafting System Premium Plugin"},{depth:2,slug:"features",text:"Features"},{depth:1,slug:"installation-instructions",text:"Installation Instructions"},{depth:3,slug:"importsts-serverside",text:"imports.ts (Serverside)"}],source:`
+# Crafting System Premium Plugin
+
+## Features
+- Add Crafting Points and Crafting Item over config file (craftingList.ts)
+- Consumables are checked if they are available
+
+# Installation Instructions
+
+- Drop the plugins files into plugins.
+- Crafting Points change over craftingList.ts
+- Item change over item.ts
+
+### imports.ts (Serverside)
+\`\`\`ts
+import './seipekm-crafting-system/index';
+\`\`\``,html:`<h1 id="crafting-system-premium-plugin">Crafting System Premium Plugin</h1>
+<h2 id="features">Features</h2>
+<ul>
+<li>Add Crafting Points and Crafting Item over config file (craftingList.ts)</li>
+<li>Consumables are checked if they are available</li>
+</ul>
+<h1 id="installation-instructions">Installation Instructions</h1>
+<ul>
+<li>Drop the plugins files into plugins.</li>
+<li>Crafting Points change over craftingList.ts</li>
+<li>Item change over item.ts</li>
+</ul>
+<h3 id="importsts-serverside">imports.ts (Serverside)</h3>
+<pre class="language-ts"><code data-astro-raw class="language-ts"><span class="token keyword">import</span> <span class="token string">'./seipekm-crafting-system/index'</span><span class="token punctuation">;</span></code></pre>`}}},n)});
