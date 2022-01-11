82a8599e882be4299857b4ca3bc13576ad4d982d
Stuyk <Stuyk@users.noreply.github.com>
2022-01-11 13:14:51 +0000

    Deploying to dist from @ Stuyk/altv-athena-plugins@4483b54b3af826bda1df51d8a5c06370d9c5c6de 🚀

---
+++ assets/AthenaFarmController.e89a783b.js
@@ -0,0 +1,31 @@
+import{_ as e}from"./preload-helper.8b70a8dd.js";import{l as n}from"./vendor.f3700654.js";n("142Aq9",{value:!0},async()=>{const[{default:t},{default:r}]=await Promise.all([e(()=>import("./MainDisplay.17de610c.js"),["assets/MainDisplay.17de610c.js","assets/Header.ef06e2f6.js","assets/vendor.f3700654.js"]),e(()=>import("./client.6ec4751d.js"),["assets/client.6ec4751d.js","assets/vendor.f3700654.js"])]);return(o,i)=>r(o)(t,{content:{title:"Athena Farming Controller",description:"Some simple farming system for the Athena Framework - Free, Customizable and Open Source!",author:"Der Lord!",version:"3.0.4+",images:["https://user-images.githubusercontent.com/82890183/148943871-d2df632e-1235-4ea5-af79-01e63857e41a.png"],keywords:["free","player","server","system","item"],url:"https://github.com/Booster1212/AthenaFarmController",discord:"Der Lord!#6021",astro:{headers:[{depth:1,slug:"athenafarmcontroller",text:"AthenaFarmController"},{depth:1,slug:"features",text:"Features"},{depth:1,slug:"setup",text:"Setup"}],source:`# AthenaFarmController
+
+![Fichier 39mdpi](https://user-images.githubusercontent.com/82890183/148942562-124cf72b-2aca-4a9d-9693-d6f5be7e35c0.png)
+
+# Features
+- Tools with durability (Build your own)
+- Build Custom Routes with custom item returns
+- Random item returns so each time you farm you'll get another item maybe
+- Build different spots
+- Anti "E-KEY" Macro
+- Random farming times
+
+# Setup
+- This is an Serverside plugin, so it has to be located at src/core/server-plugins/athena-os-farming
+- Import it by import './athena-os-farming/index'; and you are good to go.
+`,html:`<h1 id="athenafarmcontroller">AthenaFarmController</h1>
+<p><img src="https://user-images.githubusercontent.com/82890183/148942562-124cf72b-2aca-4a9d-9693-d6f5be7e35c0.png" alt="Fichier 39mdpi"></p>
+<h1 id="features">Features</h1>
+<ul>
+<li>Tools with durability (Build your own)</li>
+<li>Build Custom Routes with custom item returns</li>
+<li>Random item returns so each time you farm you\u2019ll get another item maybe</li>
+<li>Build different spots</li>
+<li>Anti \u201CE-KEY\u201D Macro</li>
+<li>Random farming times</li>
+</ul>
+<h1 id="setup">Setup</h1>
+<ul>
+<li>This is an Serverside plugin, so it has to be located at src/core/server-plugins/athena-os-farming</li>
+<li>Import it by import \u2018./athena-os-farming/index\u2019; and you are good to go.</li>
+</ul>`}}},i)});
